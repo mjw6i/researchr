739358b351b487a1e1eebad7722706b9dea01413
mjw6i <w6i@tuta.io>
2021-10-05 21:38:25 +0200

    serve over http

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"html/template"
-	"os"
+	"log"
+	"net/http"
 )
 
-func main() {
-	layout := template.Must(template.ParseFiles("index.htm"))
+var layout = template.Must(template.ParseFiles("index.htm"))
 
-	layout.Execute(os.Stdout, "")
+func main() {
+	http.HandleFunc("/", homeHandler)
+
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	layout.ExecuteTemplate(w, "index.htm", "")
 }
