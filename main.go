96519ed5db84ab05493e50f9044402af00ec1c1c
mjw6i <w6i@tuta.io>
2021-10-06 02:22:43 +0200

    extend layout

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ var layout = template.Must(template.ParseFiles("template/layout.htm", "template/
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/submit", submitHandler)
 	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
 	log.Fatal(http.ListenAndServe(":9000", nil))
 }
@@ -19,5 +20,23 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	layout.ExecuteTemplate(w, "layout.htm", "")
+	err := layout.ExecuteTemplate(w, "home.htm", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = layout.ExecuteTemplate(w, "layout.htm", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func submitHandler(w http.ResponseWriter, r *http.Request) {
+	err := layout.ExecuteTemplate(w, "submit.htm", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = layout.ExecuteTemplate(w, "layout.htm", "")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
