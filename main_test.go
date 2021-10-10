57899399c410f0959287d79459e637d1d11229de
mjw6i <w6i@tuta.io>
2021-10-10 04:14:29 +0200

    extract function

--- main_test.go
+++ main_test.go
@@ -7,15 +7,7 @@ import (
 )
 
 func TestRandomRoute(t *testing.T) {
-	req, err := http.NewRequest("GET", "/random", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	recorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(homeHandler)
-
-	handler.ServeHTTP(recorder, req)
+	recorder := makeRequest(t, homeHandler, "/random")
 
 	expectedStatus := 404
 	expectedBody := "404 page not found\n"
@@ -27,4 +19,18 @@ func TestRandomRoute(t *testing.T) {
 	if recorder.Body.String() != expectedBody {
 		t.Errorf("Expected response: '%v' got '%v'", expectedBody, recorder.Body.String())
 	}
+}
+
+func makeRequest(t *testing.T, h func(http.ResponseWriter, *http.Request), url string) *httptest.ResponseRecorder {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(h)
+
+	handler.ServeHTTP(recorder, req)
+
+	return recorder
 }
