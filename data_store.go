b6a408dcbda9966ea9798ebe9543fcefcddd68db
mjw6i <w6i@tuta.io>
2021-10-24 03:26:35 +0200

    move mocks

--- data_store.go
+++ data_store.go
@@ -42,41 +42,6 @@ type Experiment struct {
 	Wing2      bool
 }
 
-type SuccessStore struct{}
-
-func (store SuccessStore) getResult() (Result, error) {
-	result := Result{
-		RemainedResponsivePercent:         "57.03",
-		RemainedResponsiveHeadlessPercent: "1.63",
-		AverageExtremitiesRemoved:         "3.72",
-		RemainedResponsive0MissingPercent: "100",
-		RemainedResponsive1MissingPercent: "95.88",
-		RemainedResponsive2MissingPercent: "87.80",
-		RemainedResponsive3MissingPercent: "75.61",
-		RemainedResponsive4MissingPercent: "65.91",
-		RemainedResponsive5MissingPercent: "34.82",
-		RemainedResponsive6MissingPercent: "24.27",
-		RemainedResponsive7MissingPercent: "11.56",
-		RemainedResponsive8MissingPercent: "0.03",
-	}
-
-	return result, nil
-}
-
-func (store SuccessStore) storeExperiment(e Experiment) error {
-	return nil
-}
-
-type FailureStore struct{}
-
-func (store FailureStore) getResult() (Result, error) {
-	return Result{}, errors.New("Store error")
-}
-
-func (store FailureStore) storeExperiment(e Experiment) error {
-	return errors.New("Store error")
-}
-
 type DatabaseStore struct {
 	db *sql.DB
 }
